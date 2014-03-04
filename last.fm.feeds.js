5f9431eb4dca7b7b67e23c8b3b05103f4fc008a6
Christopher Kinch <kinchie@gmail.com>
2014-03-04 19:56:27 +0000

    Pluginizing the JS

--- last.fm.feeds.js
+++ last.fm.feeds.js
@@ -1,116 +1,143 @@
 /* CHRISKINCH.COM LAST FM FEED VIEWER */
-$(document).ready(function(){
-	//Getting the JSON feed from Last.FM
-	var settings									= new Object();
-	settings.username 						= 'chriskinch';
-	settings.apikey							= 'de5f3c80c1116bc51987f9aebc9fc3e9';
+(function( $ ) {
 
-	settings.getTopAlbums					= new Object();
-	settings.getTopAlbums.limit 	= '20'; //(optional): The number of results to fetch per page. Defaults to 50
-	settings.getTopAlbums.period 	= '1month'; //(optional): overall | 7day | 1month | 3month | 6month | 12month
-	settings.getTopAlbums.url 		= 'http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=' + settings.username +
-																	'&api_key=' + settings.apikey +
-																	'&limit=' + settings.getTopAlbums.limit +
-																	'&period=' + settings.getTopAlbums.period +
-																	'&format=json&callback=?';
+	$.fn.mylastfm = function( options ) {
+		//Constructing settings and defaults
+	  var defaults = {
+	  	username: 'chriskinch',
+	  	apikey: 	'de5f3c80c1116bc51987f9aebc9fc3e9',
+	  	albums: 	'20',
+	  	size: 		120,
+	  	period: 	'1month',
+	  	recent: 	'1',
+	  }
 
-	settings.getRecentTracks				= new Object();
-	settings.getRecentTracks.limit 	= '1'; //(optional): The number of results to fetch per page.
-	settings.getRecentTracks.url 		= 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + settings.username +
-																		'&api_key=' + settings.apikey +
-																		'&limit=' + settings.getRecentTracks.limit +
-																		'&format=json&callback=?';
+		var settings = $.extend( {}, defaults, options );
 
-	//http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=rj&api_key=b25b959554ed76058ac220b7b2e0a026
+	// var settings									= new Object();
+	// settings.username 						= 'chriskinch';
+	// settings.apikey							= 'de5f3c80c1116bc51987f9aebc9fc3e9';
 
-	var config = new Object();
-	config.size = 120;
+	// settings.getTopAlbums					= new Object();
+	// settings.getTopAlbums.limit 	= '20'; //(optional): The number of results to fetch per page. Defaults to 50
+	// settings.getTopAlbums.period 	= '1month'; //(optional): overall | 7day | 1month | 3month | 6month | 12month
+	// settings.getTopAlbums.url 		= 'http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=' + settings.username +
+	// 																'&api_key=' + settings.apikey +
+	// 																'&limit=' + settings.getTopAlbums.limit +
+	// 																'&period=' + settings.getTopAlbums.period +
+	// 																'&format=json&callback=?';
 
-	//choose a relavent image file size
-	var image_id = getImageScale(config.size);
+	// settings.getRecentTracks				= new Object();
+	// settings.getRecentTracks.limit 	= '1'; //(optional): The number of results to fetch per page.
+	// settings.getRecentTracks.url 		= 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + settings.username +
+	// 																	'&api_key=' + settings.apikey +
+	// 																	'&limit=' + settings.getRecentTracks.limit +
+	// 																	'&format=json&callback=?';
 
-	//START - getTopAlbums JSON load
-	$.getJSON(settings.getTopAlbums.url, function(data) {
+	// //http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=rj&api_key=b25b959554ed76058ac220b7b2e0a026
 
-		ol = $('<ol></ol>').appendTo('#lastfmrecords .albums');
+	// var config = new Object();
+	// config.size = 120;
 
-		$(data.topalbums.album).each(function(index) {
+		var topAlbumsUrl = 		'http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=' + settings.username +
+													'&api_key=' + settings.apikey +
+													'&limit=' + settings.albums +
+													'&period=' + settings.period +
+													'&format=json&callback=?';
 
-			var album_id 				= 'album-' + (index+1);
-			var album_first 		= (index == 0) ? ' first' : '';
-			var album_last 			= (index+1 == settings.getTopAlbums.limit) ? ' last' : '';
+		var recentTracksUrl = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + settings.username +
+													'&api_key=' + settings.apikey +
+													'&limit=' + settings.recent +
+													'&format=json&callback=?';
 
-			var classes_array 	= 'album ' + album_id + album_first + album_last;
-			var lfm_artist			= data.topalbums.album[index].artist.name;
-			var lfm_album				= data.topalbums.album[index].name;
-			var lfm_plays				= data.topalbums.album[index].playcount;
-			var lfm_href				= data.topalbums.album[index].url;
-			var lfm_image				= data.topalbums.album[index].image[image_id]['#text'];
+		//choose a relavent image file size
+		var image_id = getImageScale(settings.size);
 
-			li = $('<li></li>')
-				.attr('id', lfm_album.cleanup())
-				.attr('class', classes_array)
-				.appendTo(ol);
+		//START - getTopAlbums JSON load
+		$.getJSON(topAlbumsUrl, function(data) {
 
-			link = $('<a></a>')
-				.attr('href', lfm_href)
-				.attr('target', '_blank')
-				.appendTo(li);
+			ol = $('<ol></ol>').appendTo('#lastfmrecords .albums');
 
-			images = $('<img></img>')
-				.attr('src', lfm_image)
-				.attr('width', config.size)
-				.attr('height', config.size)
-				.appendTo(link);
+			$(data.topalbums.album).each(function(index) {
 
-			info = $('<span></span>')
-				.attr('class', 'info')
-				.appendTo(link);
+				var album_id 				= 'album-' + (index+1);
+				var album_first 		= (index == 0) ? ' first' : '';
+				var album_last 			= (index+1 == settings.limit) ? ' last' : '';
 
-			inner = $('<span></span>')
-				.attr('class', 'inner')
-				.appendTo(info);
+				var classes_array 	= 'album ' + album_id + album_first + album_last;
+				var lfm_artist			= data.topalbums.album[index].artist.name;
+				var lfm_album				= data.topalbums.album[index].name;
+				var lfm_plays				= data.topalbums.album[index].playcount;
+				var lfm_href				= data.topalbums.album[index].url;
+				var lfm_image				= data.topalbums.album[index].image[image_id]['#text'];
 
-			$('<span class="artist"><label>Artist:</label> ' + lfm_artist + '</span>').appendTo(inner);
-			$('<span class="album"><label>Album:</label> ' + lfm_album + '</span>').appendTo(inner);
-			$('<span class="plays">' + lfm_plays + ' <span>Plays</span></span>').appendTo(inner);
+				li = $('<li></li>')
+					.attr('id', lfm_album.cleanup())
+					.attr('class', classes_array)
+					.appendTo(ol);
 
-			//$('.info .inner').first().text("hello");
+				link = $('<a></a>')
+					.attr('href', lfm_href)
+					.attr('target', '_blank')
+					.appendTo(li);
+
+				images = $('<img></img>')
+					.attr('src', lfm_image)
+					.attr('width', settings.size)
+					.attr('height', settings.size)
+					.appendTo(link);
+
+				info = $('<span></span>')
+					.attr('class', 'info')
+					.appendTo(link);
+
+				inner = $('<span></span>')
+					.attr('class', 'inner')
+					.appendTo(info);
+
+				$('<span class="artist"><label>Artist:</label> ' + lfm_artist + '</span>').appendTo(inner);
+				$('<span class="album"><label>Album:</label> ' + lfm_album + '</span>').appendTo(inner);
+				$('<span class="plays">' + lfm_plays + ' <span>Plays</span></span>').appendTo(inner);
+
+				//$('.info .inner').first().text("hello");
+			});
 		});
-	});
-	//END - getTopAlbums JSON load
+		//END - getTopAlbums JSON load
 
-	//START - getRecentTracks JSON load
-	$.getJSON(settings.getRecentTracks.url, function(data) {
+		//START - getRecentTracks JSON load
+		$.getJSON(recentTracksUrl, function(data) {
 
-		if(data.recenttracks.track[0]) {
-			var selected_data = data.recenttracks.track[0];
-			var lfm_label	= 'Listening to:';
-			$('.eq .graph').addClass('animated');
-		} else {
-			var selected_data = data.recenttracks.track;
-			var lfm_label	= 'Recently played:';
+			if(data.recenttracks.track[0]) {
+				var selected_data = data.recenttracks.track[0];
+				var lfm_label	= 'Listening to:';
+				$('.eq .graph').addClass('animated');
+			} else {
+				var selected_data = data.recenttracks.track;
+				var lfm_label	= 'Recently played:';
+			}
+
+			var lfm_image 	= selected_data.image[image_id]['#text'];
+			var lfm_artist 	= selected_data.artist['#text'];
+			var lfm_album		= selected_data.album['#text'];
+			var lfm_track		= selected_data.name;
+			var lfm_image		= selected_data.image[image_id]['#text'];
+
+				$('<img></img>').attr('src', lfm_image).attr('class', 'cover').appendTo($('.playing'));
+			$('<span class="track"><label>' + lfm_label + '</label> ' + lfm_track + '</span>').appendTo($('.playing .inner'));
+			$('<span class="artist"><label>From:</label> ' + lfm_album + ' <label>by:</label> ' +  lfm_artist + '</span>').appendTo($('.playing .inner'));
+
+		});
+		//END - getRecentTracks JSON load
+
+		String.prototype.cleanup = function() {
+		   return this.toLowerCase().replace(/[^a-zA-Z0-9]+/g, "-");
 		}
 
-		var lfm_image 	= selected_data.image[image_id]['#text'];
-		var lfm_artist 	= selected_data.artist['#text'];
-		var lfm_album		= selected_data.album['#text'];
-		var lfm_track		= selected_data.name;
-		var lfm_image		= selected_data.image[image_id]['#text'];
+		function getImageScale(size) {
+			var index = (size<=34) ? 0 : (size <= 64) ? 1 : (size <= 126) ? 2 : 3;
+			return index;
+		}
 
- 		$('<img></img>').attr('src', lfm_image).attr('class', 'cover').appendTo($('.playing'));
-		$('<span class="track"><label>' + lfm_label + '</label> ' + lfm_track + '</span>').appendTo($('.playing .inner'));
-		$('<span class="artist"><label>From:</label> ' + lfm_album + ' <label>by:</label> ' +  lfm_artist + '</span>').appendTo($('.playing .inner'));
+	};
 
-	});
-	//END - getRecentTracks JSON load
-
-	String.prototype.cleanup = function() {
-	   return this.toLowerCase().replace(/[^a-zA-Z0-9]+/g, "-");
-	}
-
-	function getImageScale(size) {
-		var index = (size<=34) ? 0 : (size <= 64) ? 1 : (size <= 126) ? 2 : 3;
-		return index;
-	}
-});
+})( jQuery );
