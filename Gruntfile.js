b7362d224c460bfb32b0a0c0658c46fd714869b8
Chris Kinch <kinchie@gmail.com>
2014-07-09 11:07:04 +0100

    repo cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,62 @@
 module.exports = function(grunt) {
+  'use strict';
 
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+
+    meta: {
+      banner: {
+        dist: '/*!\n'+
+               ' * <%= pkg.name %> v<%= pkg.version %> - <%= pkg.description %>\n'+
+               ' * <%= pkg.repository.url %>\n'+
+               ' */\n'
       },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'dist/<%= pkg.name %>.min.js'
+      outputDir: 'dist',
+      output : '<%= meta.outputDir %>/<%= pkg.name %>',
+      outputMin : '<%= meta.outputDir %>/<%= pkg.name.replace("js", "min.js") %>'
+    },
+
+    concat: {
+      options: {
+        separator: ''
+      },
+      dist: {
+        src: [
+          'src/last.fm.feeds.js'
+        ],
+        dest: '<%= meta.output %>.js'
       }
-    }
+    },
+
+    uglify: {
+      dist: {
+        options: {
+          banner: '<%= meta.banner.dist %>'
+        },
+        files: {
+          '<%= meta.outputDir %>/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      },
+    },
+
+    jshint: {
+      options: {
+        jshintrc : '.jshintrc'
+      },
+      files: ['Gruntfile.js', 'src/']
+    },
+
+
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('default', ['concat','uglify']);
 
 };
