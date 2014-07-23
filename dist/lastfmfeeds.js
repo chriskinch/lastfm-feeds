d6ae8f86a310e6d7fdb23bf6565eb0151464ff9d
Chris Kinch <kinchie@gmail.com>
2014-07-23 19:43:26 +0100

    Adding destroy method

--- dist/lastfmfeeds.js
+++ dist/lastfmfeeds.js
@@ -21,15 +21,15 @@
   * @param {String} method: The type of API to call to be made. (e.g: user.gettopalbums|user.getrecenttracks)
   *
   * @param {Object} options: Options object (Optional) 
-  * @param {Object} options.limit: The number of results to fetch per page. Defaults to 10.
-  * @param {Object} options.size: The size of the albumb art to return.
-  * @param {Object} options.period: The time period over which to retrieve top albums for (e.g: overall|7day|1month|3month|6month|12month)
-  * @param {Object} options.cover: Toggles the rendering of the cover image
-  * @param {Object} options.album: Toggles the rendering of the album name
-  * @param {Object} options.artist: Toggles the rendering of the artist name
-  * @param {Object} options.plays: Toggles the rendering of the play count
-  * @param {Object} options.date: Toggles the rendering of the date played
-  * @param {Object} options.playing: Toggles the rendering of the current playing track (note: user.getrecenttracks only)
+  * @param {Number} options.limit: The number of results to fetch per page. Defaults to 10.
+  * @param {Number} options.size: The size of the albumb art to return.
+  * @param {String} options.period: The time period over which to retrieve top albums for (e.g: overall|7day|1month|3month|6month|12month)
+  * @param {Boolean} options.cover: Toggles the rendering of the cover image
+  * @param {Boolean} options.album: Toggles the rendering of the album name
+  * @param {Boolean} options.artist: Toggles the rendering of the artist name
+  * @param {Boolean} options.plays: Toggles the rendering of the play count
+  * @param {Boolean} options.date: Toggles the rendering of the date played
+  * @param {Boolean} options.playing: Toggles the rendering of the current playing track (note: user.getrecenttracks only)
   */
 
   // All currently instantiated instances of feeds
@@ -45,10 +45,10 @@
 
   	init: function( selector, user, api_key, method, options ){
   		var instance = {};
-  		// reference to the jQuery version of DOM element
+  		// reference to the jQuery version of DOM element.
   		instance.element = $(selector);
 
-  		// Setup variables and defaults
+  		// Setup variables and defaults.
   		var defaults = {
   				limit:		10,
   				size:		64,
@@ -61,9 +61,10 @@
   				playing:	true,
   			};
 
-  		// Final properties and options are merged to default
+  		// Final properties and options are merged to default.
   		instance.settings = $.extend({}, defaults, options);
 
+  		// Setting up JSOM config from provided params and settings.
   		instance.config = {
   			url:'http://ws.audioscrobbler.com/2.0/?',
   			params: {
@@ -83,14 +84,27 @@
   		var feed = new FeedLoader( instance.element, instance.settings );
   			feed.loadFeed( instance.config.url, instance.config.params );
 
+  		// Saving instance to array for later use.
   		ALL_INSTANCES[selector] = instance;
 
   	},
 
-      refresh: function( selectors ) {
-  		var self = this;
+  	destroy: function( selectors ) {
+  		// Loop through selectors provided. If null destroy all.
+  		var instances = (selectors)? selectors : ALL_INSTANCES;
+  		$.each(instances, function(index, value) {
+  			var instance = ALL_INSTANCES[value];
+  			var element = $(value);
+  			element.trigger('lastfmfeeds:destroy');
+  			
+  			console.log(value);
+  			//element.[JQUERYY EMTPTY HERE];
+  		});
+  	},
 
+      refresh: function( selectors ) {
   		$.each(selectors, function(index, value) {
+  			this.destroy(value);
   			var instance = ALL_INSTANCES[value];
   			var element = $(value);
   			element.trigger('lastfmfeeds:refresh');
